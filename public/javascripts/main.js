1262e5200d982fd15ff68bbc90938ded33b83cbc
Trey Huffine <treyhuffine@gmail.com>
2015-07-06 10:07:07 -0700

    Serve data

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,23 @@
 var app = angular.module('tableizer', []);
 
-app.controller('MainCtrl', function($scope) {
-  $scope.hello = "HELLO WORLD";
+app
+.constant("FEAT_URL", {
+  "API_URL": "http://localhost:3000"
+})
+.service('Feature', function($http, FEAT_URL) {
+  this.getFeatures = function() {
+    return $http.get(FEAT_URL.API_URL + "/data");
+  };
+})
+.controller('MainCtrl', function($scope, Feature) {
+  $scope.hello = "Hello WOOORLD!";
+  Feature.getFeatures()
+    .success(function(data) {
+      console.log(data);
+      $scope.features = data;
+      console.log($scope.feaures);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 });
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,13 @@ var router = express.Router();
 var fs = require('fs');
 var data = require('../data.json');
 
+console.log(data);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.get('/data', function(req, res, next) {
+  res.json(data.features);
 });
 
 module.exports = router;
